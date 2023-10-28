4cb14b14038ee5ac7052765025a8d2e3dfa37a6e
ShodaDev <shoda@pwsdev.it>
2023-10-28 22:16:33 +0200

    Edit: index.php

--- resources/index.php
+++ resources/index.php
@@ -4,19 +4,18 @@ include_once BASE_PATH.'app/ConnectDB.php';
 $db = new MySQL();
 $mysql = $db->getConnection();
 
-   $SQL_GET_RANDOM_VOCAB = "SELECT * FROM translations`";
-   $SQL_GET_RANDOM_VOCAB = $mysql->prepare($SQL_GET_RANDOM_VOCAB);
-   $SQL_GET_RANDOM_VOCAB->execute();
-   
-   if ($SQL_GET_RANDOM_VOCAB->rowCount > 0) {
-       while($row = $SQL_GET_RANDOM_VOCAB->fetch_assoc()) {
-           $german = $row["german_translations"];
-           $english = $row["english_term"];
-           echo $english;
-         }
-   } else {
-       echo "Keine Vokabeln gefunden.";
-   }
+$SQL_GET_RANDOM_VOCAB = "SELECT * FROM translations"; // Der SQL-Query war fehlerhaft.
+$SQL_GET_RANDOM_VOCAB = $mysql->prepare($SQL_GET_RANDOM_VOCAB);
+$SQL_GET_RANDOM_VOCAB->execute();
+
+if ($SQL_GET_RANDOM_VOCAB->rowCount() > 0) {
+    while ($row = $SQL_GET_RANDOM_VOCAB->fetch()) { // Du musst "fetch" verwenden, nicht "fetch_assoc".
+        $german = $row["german_translation"]; // Der Spaltenname war fehlerhaft.
+        $english = $row["english_term"];
+        echo $english;
+    }
+} else {
+    echo "Keine Vokabeln gefunden.";
+}
 ?>
 <h1>Vokabeltrainer</h1>
-<?= $english; ?>
