a268adb992ec74ae5267abc44010f5f2d367478f
ShodaDev <shoda@pwsdev.it>
2023-10-28 22:42:38 +0200

    Edit: index.php

--- resources/index.php
+++ resources/index.php
@@ -1,38 +1,33 @@
 <?php
-   include_once BASE_PATH . 'app/ConnectDB.php';
+include_once BASE_PATH . 'app/ConnectDB.php';
 
-   // Starte die Session
-   session_start();
+// Starte die Session
+session_start();
 
-   $db = new MySQL();
-   $mysql = $db->getConnection();
+$db = new MySQL();
+$mysql = $db->getConnection();
 
-   $_SESSION['usedIds'] = isset($_SESSION['usedIds']) ? $_SESSION['usedIds'] : array();
+$_SESSION['usedIds'] = isset($_SESSION['usedIds']) ? $_SESSION['usedIds'] : array();
 
-    $SQL_GET_RANDOM_VOCAB = "SELECT * FROM translations ORDER BY RAND() LIMIT 1";
-    $SQL_GET_RANDOM_VOCAB = $mysql->prepare($SQL_GET_RANDOM_VOCAB);
-    $SQL_GET_RANDOM_VOCAB->execute();
+$vocabid = null;
 
-    $vocab = $SQL_GET_RANDOM_VOCAB->fetch();
-    $vocabid = $vocab['id'];
-    if(isset($_SESSION['usedIds'])) {
-        if (in_array($vocabid, $_SESSION['usedIds'])) {
-            //loop (get new vocab)
-            $SQL_GET_RANDOM_VOCAB = "SELECT * FROM translations ORDER BY RAND() LIMIT 1";
-            $SQL_GET_RANDOM_VOCAB = $mysql->prepare($SQL_GET_RANDOM_VOCAB);
-            $SQL_GET_RANDOM_VOCAB->execute();
-            $vocab = $SQL_GET_RANDOM_VOCAB->fetch();
+if (isset($_SESSION['usedIds'])) {
+    do {
+        $SQL_GET_RANDOM_VOCAB = "SELECT * FROM translations WHERE id NOT IN (" . implode(",", $_SESSION['usedIds']) . ") ORDER BY RAND() LIMIT 1";
+        $SQL_GET_RANDOM_VOCAB = $mysql->prepare($SQL_GET_RANDOM_VOCAB);
+        $SQL_GET_RANDOM_VOCAB->execute();
+        $vocab = $SQL_GET_RANDOM_VOCAB->fetch();
+        if ($vocab) {
             $vocabid = $vocab['id'];
+            $_SESSION['usedIds'][] = $vocabid;
             echo $vocab['english_term'];
-            array_push($_SESSION['usedIds'], $vocabid);
-        } else {
-            echo $vocab['english_term'];
-            array_push($_SESSION['usedIds'], $vocabid);
         }
-    } else {
-      echo $vocab['english_term'];
-      array_push($_SESSION['usedIds'], $vocabid);
-   }
+    } while (!$vocab && count($_SESSION['usedIds']) < $totalVocabCount); // Wiederhole, bis eine neue Vokabel gefunden wird oder alle Vokabeln verwendet wurden
+}
+
+if (count($_SESSION['usedIds']) == $totalVocabCount) {
+    echo "Alle Vokabeln wurden verwendet.";
+}
 ?>
 <h1>Vokabeltrainer</h1>
 
