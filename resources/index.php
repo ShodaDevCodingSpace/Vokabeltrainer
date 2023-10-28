bd85ce2a543368560a4c3e0bd4b082af84b4d6e4
ShodaDev <shoda@pwsdev.it>
2023-10-28 22:15:32 +0200

    Edit: index.php

--- resources/index.php
+++ resources/index.php
@@ -4,11 +4,19 @@ include_once BASE_PATH.'app/ConnectDB.php';
 $db = new MySQL();
 $mysql = $db->getConnection();
 
-$SQL_GET_RANDOM_VOCAB = "SELECT * FROM `translations`";
-$SQL_GET_RANDOM_VOCAB = $mysql->prepare($SQL_GET_RANDOM_VOCAB);
-$SQL_GET_RANDOM_VOCAB->execute();
-
-// Um die Anzahl der Zeilen (Datensätze) im Ergebnis zu erhalten, solltest du rowCount verwenden, nicht num_rows.
-echo $SQL_GET_RANDOM_VOCAB->rowCount();
+   $SQL_GET_RANDOM_VOCAB = "SELECT * FROM translations`";
+   $SQL_GET_RANDOM_VOCAB = $mysql->prepare($SQL_GET_RANDOM_VOCAB);
+   $SQL_GET_RANDOM_VOCAB->execute();
+   
+   if ($SQL_GET_RANDOM_VOCAB->rowCount > 0) {
+       while($row = $SQL_GET_RANDOM_VOCAB->fetch_assoc()) {
+           $german = $row["german_translations"];
+           $english = $row["english_term"];
+           echo $english;
+         }
+   } else {
+       echo "Keine Vokabeln gefunden.";
+   }
 ?>
 <h1>Vokabeltrainer</h1>
+<?= $english; ?>
