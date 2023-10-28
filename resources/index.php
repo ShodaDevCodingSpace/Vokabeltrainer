c6b9fa5450ea917abd2ed2c0b8768ff0fe3e198a
ShodaDev <shoda@pwsdev.it>
2023-10-28 22:21:34 +0200

    Edit: index.php

--- resources/index.php
+++ resources/index.php
@@ -8,11 +8,18 @@ $SQL_GET_RANDOM_VOCAB = "SELECT * FROM translations ORDER BY RAND() LIMIT 1"; //
 $SQL_GET_RANDOM_VOCAB = $mysql->prepare($SQL_GET_RANDOM_VOCAB);
 $SQL_GET_RANDOM_VOCAB->execute();
 
+$maxId = $SQL_GET_RANDOM_VOCAB->rowCount();
+
+$usedIds = new Array();
+
 if ($SQL_GET_RANDOM_VOCAB->rowCount() > 0) {
     while ($row = $SQL_GET_RANDOM_VOCAB->fetch()) { // Du musst "fetch" verwenden, nicht "fetch_assoc".
         $german = $row["german_translation"]; // Der Spaltenname war fehlerhaft.
         $english = $row["english_term"];
+        $id = $row['id']:
         echo $english;
+
+        $usedIds->push($id);
     }
 } else {
     echo "Keine Vokabeln gefunden.";
