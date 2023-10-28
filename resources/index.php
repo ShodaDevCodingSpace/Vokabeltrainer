e3a635456f334bea636f69c98bf21911c326cc31
ShodaDev <shoda@pwsdev.it>
2023-10-28 22:25:54 +0200

    Edit: index.php

--- resources/index.php
+++ resources/index.php
@@ -4,25 +4,22 @@ include_once BASE_PATH.'app/ConnectDB.php';
 $db = new MySQL();
 $mysql = $db->getConnection();
 
-$SQL_GET_RANDOM_VOCAB = "SELECT * FROM translations ORDER BY RAND() LIMIT 1";
+$SQL_GET_RANDOM_VOCAB = "SELECT * FROM translations ORDER BY RAND() LIMIT 1"; // Der SQL-Query war fehlerhaft.
 $SQL_GET_RANDOM_VOCAB = $mysql->prepare($SQL_GET_RANDOM_VOCAB);
 $SQL_GET_RANDOM_VOCAB->execute();
 
-// Verwende "rowCount()" nicht, um die Anzahl der Ergebnisse zu zählen.
-// Stattdessen verwenden wir "rowCount()" für Statements, die Daten ändern (INSERT, UPDATE, DELETE).
-// Verwende stattdessen "rowCount()" bei einem SELECT-Statement.
-$rowCount = $SQL_GET_RANDOM_VOCAB->rowCount();
+$maxId = $SQL_GET_RANDOM_VOCAB->rowCount();
 
-$usedIds = array(); // Du solltest "array()" statt "new Array()" verwenden.
+$usedIds = array();
 
-if ($rowCount > 0) {
-    while ($row = $SQL_GET_RANDOM_VOCAB->fetch()) { // "fetch_assoc()" ist nicht notwendig, da du die Spalten über ihren Namen ansprechen möchtest.
-        $german = $row["german_translation"];
+if ($SQL_GET_RANDOM_VOCAB->rowCount() > 0) {
+    while ($row = $SQL_GET_RANDOM_VOCAB->fetch()) { // Du musst "fetch" verwenden, nicht "fetch_assoc".
+        $german = $row["german_translation"]; // Der Spaltenname war fehlerhaft.
         $english = $row["english_term"];
-        $id = $row['id']; // Ändere ":" zu ";".
+        $id = $row['id'];
         echo $english;
 
-        $usedIds[] = $id; // Verwende [] für das Hinzufügen eines Elements zu einem Array.
+        $usedIds[] = $id;
     }
 } else {
     echo "Keine Vokabeln gefunden.";
