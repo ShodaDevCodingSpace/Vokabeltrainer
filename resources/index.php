af59c754dd01afc68bdcbd6021b894b95e688221
ShodaDev <shoda@pwsdev.it>
2023-10-28 22:00:21 +0200

    Edit: index.php

--- resources/index.php
+++ resources/index.php
@@ -1,1 +1,16 @@
+<?php
+   $SQL_GET_RANDOM_VOCAB = "SELECT * FROM vokabeln ORDER BY RAND() LIMIT 10";
+   $SQL_GET_RANDOM_VOCAB = $mysql->prepare($SQL_GET_RANDOM_VOCAB);
+   $SQL_GET_RANDOM_VOCAB->execute();
+   
+   if ($SQL_GET_RANDOM_VOCAB->num_rows > 0) {
+       while($row = $SQL_GET_RANDOM_VOCAB->fetch_assoc()) {
+           $deutsch = $row["deutsch"];
+           $englisch = $row["englisch"];
+         }
+   } else {
+       echo "Keine Vokabeln gefunden.";
+   }
+?>
 <h1>Vokabeltrainer</h1>
+<?= $englisch ?>
