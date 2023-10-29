59c807fda3c11e8c2878fa8ebf97f9658b2773b1
ShodaDev <shoda@pwsdev.it>
2023-10-29 07:48:38 +0100

    Edit: learnVocabs.php

--- resources/learnVocabs.php
+++ resources/learnVocabs.php
@@ -1,4 +1,12 @@
 <?php
    session_start();
    echo $_SESSION['maxVocabs'];
+   
+   if(isset($_POST['endsession'])) {
+      session_destroy();
+      header("Location: https://shoda.lol");
+   }
 ?>
+<form method="POST">
+   <input type="submit" name="endsession" value="Session beenden">
+</form>
