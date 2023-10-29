2acfd94d5b353680debfae245d5de4b1ce467973
ShodaDev <shoda@pwsdev.it>
2023-10-29 07:21:56 +0100

    Edit file sys

---
+++ app/getRandomVocabs.php
@@ -0,0 +1,35 @@
+<?php
+include_once BASE_PATH . 'app/ConnectDB.php';
+
+session_start();
+
+$db = new MySQL();
+$mysql = $db->getConnection();
+
+$_SESSION['usedIds'] = isset($_SESSION['usedIds']) ? $_SESSION['usedIds'] : array();
+
+$vocabid = null;
+$totalVocabCount = 9;
+
+if (count($_SESSION['usedIds']) < $totalVocabCount) {
+   do {
+       $SQL_GET_RANDOM_VOCAB = "SELECT * FROM translations";
+       if (!empty($_SESSION['usedIds'])) {
+           $SQL_GET_RANDOM_VOCAB .= " WHERE id NOT IN (" . implode(',', $_SESSION['usedIds']) . ")";
+       }
+       $SQL_GET_RANDOM_VOCAB .= " ORDER BY RAND() LIMIT 1";
+       $SQL_GET_RANDOM_VOCAB = $mysql->prepare($SQL_GET_RANDOM_VOCAB);
+       $SQL_GET_RANDOM_VOCAB->execute();
+       $vocab = $SQL_GET_RANDOM_VOCAB->fetch();
+       if ($vocab) {
+           $vocabid = $vocab['id'];
+           $_SESSION['usedIds'][] = $vocabid;
+           return $vocab['english_term'];
+       }
+   } while (!$vocab && count($_SESSION['usedIds']) < $totalVocabCount);
+}
+
+if (count($_SESSION['usedIds']) > $totalVocabCount) {
+    return "Alle Vokabeln wurden verwendet.";
+}
+?>
--- resources/index.php
+++ resources/index.php
@@ -1,42 +1,4 @@
 <?php
-include_once BASE_PATH . 'app/ConnectDB.php';
-
-// Starte die Session
-session_start();
-
-$db = new MySQL();
-$mysql = $db->getConnection();
-
-// Stelle sicher, dass $_SESSION['usedIds'] initialisiert ist
-$_SESSION['usedIds'] = isset($_SESSION['usedIds']) ? $_SESSION['usedIds'] : array();
-
-$vocabid = null;
-$totalVocabCount = 9; // Annahme: Du möchtest insgesamt 10 Vokabeln verwenden.
-
-if (count($_SESSION['usedIds']) < $totalVocabCount) {
-   do {
-       $SQL_GET_RANDOM_VOCAB = "SELECT * FROM translations";
-       if (!empty($_SESSION['usedIds'])) {
-           $SQL_GET_RANDOM_VOCAB .= " WHERE id NOT IN (" . implode(',', $_SESSION['usedIds']) . ")";
-       }
-       $SQL_GET_RANDOM_VOCAB .= " ORDER BY RAND() LIMIT 1";
-       $SQL_GET_RANDOM_VOCAB = $mysql->prepare($SQL_GET_RANDOM_VOCAB);
-       $SQL_GET_RANDOM_VOCAB->execute();
-       $vocab = $SQL_GET_RANDOM_VOCAB->fetch();
-       if ($vocab) {
-           $vocabid = $vocab['id'];
-           $_SESSION['usedIds'][] = $vocabid;
-           echo $vocab['english_term'];
-       }
-   } while (!$vocab && count($_SESSION['usedIds']) < $totalVocabCount);
-}
-
-if (count($_SESSION['usedIds']) > $totalVocabCount) {
-    echo "Alle Vokabeln wurden verwendet.";
-}
+include BASE_PATH . 'app/getRandomVocabs.php';
 ?>
 <h1>Vokabeltrainer</h1>
-
-<?php
-print_r($_SESSION['usedIds']);
-?>
