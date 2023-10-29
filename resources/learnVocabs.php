f3400cbb892c74908af1caca15325677d130647a
ShodaDev <shoda@pwsdev.it>
2023-10-29 09:39:42 +0100

    Test

--- resources/learnVocabs.php
+++ resources/learnVocabs.php
@@ -15,7 +15,7 @@
    include BASE_PATH . 'app/getRandomVocabs.php';
 ?>
 
-<?= $vocabs; ?>
+<?php print_r $vocabs; ?>
 
 <form method="POST">
    <input type="text" name="enteredVocab" placeholder="Übersetzung hier eingeben" required>
