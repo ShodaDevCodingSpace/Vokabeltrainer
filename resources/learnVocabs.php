f382136d19b29e6a53102206089ca499828bcc54
ShodaDev <shoda@pwsdev.it>
2023-10-29 08:22:04 +0100

    Edit: getRandomVocabs.php

--- resources/learnVocabs.php
+++ resources/learnVocabs.php
@@ -8,7 +8,17 @@
       header("Location: https://shoda.lol");
    }
 ?>
+<?php
+   if(!count$_SESSION['usedIds']) === $_SESSION['maxVocabs']) {
+      ?>
 <p><?= count(isset($_SESSION['usedIds']) ? $_SESSION['usedIds'] : array()) + 1; ?>/<?= $_SESSION['maxVocabs']; ?></p>
+<?php 
+   } else {
+      ?>
+<p><?= $_SESSION['maxVocabs']; ?>/<?= $_SESSION['maxVocabs']; ?></p>
+<?php
+   }
+?>
 <?php
    include BASE_PATH . 'app/getRandomVocabs.php';
 ?>
