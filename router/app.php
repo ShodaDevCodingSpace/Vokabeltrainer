1b48db0d9cd2faccb59b7fe6e9cfc75802ecf9a5
ShodaDev <shoda@pwsdev.it>
2023-10-29 07:36:23 +0100

    Edit: index.php

--- router/app.php
+++ router/app.php
@@ -3,7 +3,15 @@
 require_once BASE_PATH . 'Controllers/BaseController.php';
 $router = new Router();
 
+//GET /
 $router->register('/', new BaseController('index'), 'GET');
+//POST /
+$router->register('/', new BaseController('index'), 'POST');
 
+
+//GET /learnVocabs
+$router->register('/learnVocabs', new BaseController('learnVocabs'), 'GET');
+//POST /learnVocabs
+$router->register('/learnVocabs', new BaseController('learnVocabs'), 'POST');
 
 return $router;
