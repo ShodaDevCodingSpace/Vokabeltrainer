10ec1f81cd269273a769e5d89e867855b2aab674
ShodaDev <shoda@pwsdev.it>
2023-10-29 09:42:17 +0100

    Test

--- resources/learnVocabs.php
+++ resources/learnVocabs.php
@@ -15,7 +15,7 @@
    include BASE_PATH . 'app/getRandomVocabs.php';
 ?>
 
-<?php print_r($vocabs); ?>
+<?= $vocabs[$_SESSION['usedIds']]->english; ?>
 
 <form method="POST">
    <input type="text" name="enteredVocab" placeholder="Übersetzung hier eingeben" required>
