e8ff466a6f5429355768a1a1316f284ed6855898
ShodaDev <shoda@pwsdev.it>
2023-10-29 07:27:50 +0100

    Edit: index.php

--- resources/index.php
+++ resources/index.php
@@ -2,4 +2,8 @@
 include BASE_PATH . 'app/getRandomVocabs.php';
 ?>
 <h1>Vokabeltrainer</h1>
-<?= $response; ?>
+<p>Wie viele Vokabeln willst du lernen?</p>
+<form method="POST">
+   <input type="button" name="10" value="10">
+   <input type="button" name="20" value="20">
+</form>
