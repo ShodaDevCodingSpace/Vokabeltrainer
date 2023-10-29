d9bb85e7b1add9824d6cffc84882ae3e55b8cfce
ShodaDev <shoda@pwsdev.it>
2023-10-29 07:47:54 +0100

    Changed File Sys

--- resources/index.php
+++ resources/index.php
@@ -13,6 +13,10 @@
       $_SESSION['maxVocabs'] = 25;
       header("Location: https://shoda.lol/learnVocabs");
    }
+   if(isset($_POST['endsession'])) {
+      session_destroy();
+      header("Location: https://shoda.lol");
+   }
 ?>
 
 <h1>Vokabeltrainer</h1>
@@ -21,4 +25,5 @@
    <input type="submit" name="10" value="10">
    <input type="submit" name="20" value="20">
    <input type="submit" name="25" value="25">
+   <input type="submit" name="endsession" value="Session beenden">
 </form>
