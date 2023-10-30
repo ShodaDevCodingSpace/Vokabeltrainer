8a207fee7e5744ba16752bd32fed219f4f66d4dc
ShodaDev <shoda@pwsdev.it>
2023-10-30 09:01:25 +0100

    Edit: BaseController

--- Controllers/BaseController.php
+++ Controllers/BaseController.php
@@ -8,7 +8,6 @@ class BaseController
         $this->file = $file;
     }
     public function __invoke(Request $request){
-        '<!DOCTYPE html>'
         require_once BASE_PATH . 'resources/' . $this->file . '.php';
     }
 }
