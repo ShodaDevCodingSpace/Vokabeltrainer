cf333acf43468317fa1f374a1f32de0ad52efeed
ShodaDev <shoda@pwsdev.it>
2023-11-04 13:28:39 +0100

    edit learnVocabs

--- resources/learnVocabsResult.php
+++ resources/learnVocabsResult.php
@@ -1,20 +1,27 @@
 <?php
    session_start();
-   
+
    $trueVocabsCount = $_SESSION['trueVocabsCount'];
    $falseVocabsCount = $_SESSION['falseVocabsCount'];
    $trueVocabs = $_SESSION['trueVocabs'];
    $falseVocabs = $_SESSION['falseVocabs'];
+   $trueVocabsReturn = array();
+   $falseVocabsReturn = array();
 
    //result
    $resultCount = "
       Richtig: $trueVocabsCount/10 <br>
       Falsch: $falseVocabsCount/10 <br>
    ";
-
+   while($trueVocabsCount < $_SESSION['maxVocabs']) {
+      array_push($trueVocabsReturn, $trueVocabs[$trueVocabsCount]);
+   }
+   while($falseVocabsCount < $_SESSION['maxVocabs']) {
+      array_push($falseVocabsReturn, $falseVocabs[$falseVocabsCount]);
+   }
    $result = "
-      Richtig: $trueVocabs <br>
-      Falsch: $falseVocabs <br>
+      Richtig: $trueVocabsReturn <br>
+      Falsch: $falseVocabsReturn <br>
    ";
 ?>
 
