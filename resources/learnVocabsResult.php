3455d52601f74609368e6abd0aa91445cbf41b2f
ShodaDev <shoda@pwsdev.it>
2023-11-05 10:23:29 +0100

    edit learnVocabs

--- resources/learnVocabsResult.php
+++ resources/learnVocabsResult.php
@@ -6,30 +6,22 @@ $falseVocabsCount = $_SESSION['falseVocabsCount'];
 $trueVocabs = $_SESSION['trueVocabs'];
 $falseVocabs = $_SESSION['falseVocabs'];
 
-// Initialisieren Sie die Arrays direkt mit leeren Arrays.
 $trueVocabsReturn = [];
 $falseVocabsReturn = [];
 
-// Ergebnis-Zeichenfolgen
 $resultCount = "Richtig: $trueVocabsCount/10 <br>Falsch: $falseVocabsCount/10<br>";
 
 // Kopieren Sie die richtigen und falschen Antworten in die entsprechenden Arrays.
-while ($trueVocabsCount < $_SESSION['maxVocabs']) {
-    $trueVocabsReturn[] = $trueVocabs[$trueVocabsCount];
-    $trueVocabsCount++; // Zähler erhöhen
-}
-while ($falseVocabsCount < $_SESSION['maxVocabs']) {
-    $falseVocabsReturn[] = $falseVocabs[$falseVocabsCount];
-    $falseVocabsCount++; // Zähler erhöhen
+for ($i = 0; $i < $trueVocabsCount; $i++) {
+    $trueVocabsReturn[] = $trueVocabs[$i]['english'] . ' (' . $trueVocabs[$i]['german'] . ')';
 }
 
-// Ergebnis-Zeichenfolgen für die Antworten
+for ($i = 0; $i < $falseVocabsCount; $i++) {
+    $falseVocabsReturn[] = $falseVocabs[$i]['english'] . ' (' . $falseVocabs[$i]['german'] . ')';
+}
+
 $result = "Richtig: " . implode(', ', $trueVocabsReturn) . "<br>Falsch: " . implode(', ', $falseVocabsReturn) . "<br>";
 
-var_dump($_SESSION['trueVocabsCount']);
-var_dump($_SESSION['falseVocabsCount']);
-var_dump($_SESSION['trueVocabs']);
-var_dump($_SESSION['falseVocabs']);
 ?>
 
 <?= $resultCount ?>
